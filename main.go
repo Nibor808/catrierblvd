721f47d0fe1474cb9bb1cc0d64741b19dd74b19a
Robin Erickson <re@incuboweb.ca>
2023-03-12 10:53:10 -0400

    add air and amber

--- main.go
+++ main.go
@@ -1,1 +1,29 @@
-package cartierblvd
+package main
+
+import (
+	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/template/amber"
+)
+
+func main() {
+	engine := amber.New("./views", ".amber")
+	config := fiber.Config{
+		AppName:            "CartierBlvd",
+		EnableIPValidation: true,
+		EnablePrintRoutes:  true,
+		Views:              engine,
+	}
+
+	app := fiber.New(config)
+
+	app.Get("/", func(c *fiber.Ctx) error {
+		return c.Render("index", fiber.Map{
+			"Title": "Saturday Night At The Movies",
+		})
+	})
+
+	err := app.Listen(":3000")
+	if err != nil {
+		return
+	}
+}
