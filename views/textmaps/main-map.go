7fb0d1e2911c69e2e63a4a80ade40a7ff2767bde
Robin Erickson <re@incuboweb.ca>
2023-04-08 12:26:10 -0400

    change image; add date and new styles

--- views/textmaps/main-map.go
+++ views/textmaps/main-map.go
@@ -1,7 +1,24 @@
 package textmaps
 
-import "github.com/gofiber/fiber/v2"
+import (
+	"fmt"
+	"github.com/gofiber/fiber/v2"
+	"time"
+)
+
+func getMovieDate() string {
+	currentTime := time.Now()
+	year := currentTime.Year()
+	month := currentTime.Month()
+	day := currentTime.Day()
+	return fmt.Sprintln(month, day, year)
+}
 
 var MainMap = fiber.Map{
-	"Title": "Saturday Night At The Movies",
+	"Title":      "Saturday Night At The Movies",
+	"Date":       getMovieDate(),
+	"Intro":      "This weeks movie",
+	"MovieTitle": "The Conjuring",
+	"ShowInfo":   "Show time 8:30pm sharp",
+	"EventInfo":  "BYO beverages and seating. Spots are first come first serve.",
 }
