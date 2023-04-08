bb593a3f2ddb33c8fba173b6c21bb76f59f1033d
Robin Erickson <re@incuboweb.ca>
2023-04-08 11:01:10 -0400

    add fonts and background and other styling

--- views/textmaps/main-map.go
+++ views/textmaps/main-map.go
@@ -3,6 +3,5 @@ package textmaps
 import "github.com/gofiber/fiber/v2"
 
 var MainMap = fiber.Map{
-	"Title":         "Saturday Night At The Movies",
-	"MainTextIntro": "This is the main text intro.",
+	"Title": "Saturday Night At The Movies",
 }
